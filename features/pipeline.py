50f9f23e73b900afa0aa03277a934c581466ec25
Florian Nachtigall <nachtigall.florian@gmail.com>
2024-11-21 18:47:30 +0100

    Add buffer and street features to pipeline
    Minor changes:
    * Use snake_case instead of CamelCase for feature names
    * Add streets csv file to git lfs for testing
    * Add utils func to load street files
    * Restructure imports: first standard libs, then third-party, lastly local modules

--- features/pipeline.py
+++ features/pipeline.py
@@ -1,29 +1,66 @@
-from log import setup_logger, LoggingContext
-from util import load_buildings
 from momepy import longest_axis_length, elongation, convexity, orientation, corners, shared_walls
+
+from log import setup_logger, LoggingContext
+from util import load_buildings, load_streets
+from features import building, buffer, street
 
 
 def execute_feature_pipeline(city_path: str, log_file: str):
     logger = setup_logger(log_file=log_file)
 
     buildings = load_buildings(city_path)
+    streets = load_streets(city_path)
 
-    with LoggingContext(logger, feature_name="Building"):
-        from features import building
+    with LoggingContext(logger, feature_name='building'):
+        buildings = _calculate_building_features(buildings)
 
-        buildings['FootprintArea'] = buildings.geometry.area
-        buildings['Perimeter'] = buildings.geometry.length
-        buildings['Phi'] = building.calculate_phi(buildings)
-        buildings['LongestAxisLength'] = longest_axis_length(buildings)
-        buildings['Elongation'] = elongation(buildings)
-        buildings['Convexity'] = convexity(buildings)
-        buildings['Orientation'] = orientation(buildings)
-        buildings['Corners'] = corners(buildings)
-        buildings['SharedWallLength'] = shared_walls(buildings)
-        buildings['Touches'] = building.calculate_touches(buildings)
+    with LoggingContext(logger, feature_name='building_buffer'):
+        buildings = _calculate_building_buffer_features(buildings)
+
+    with LoggingContext(logger, feature_name='street'):
+        buildings = _calculate_street_features(buildings, streets)
 
 
-if __name__ == "__main__":
-    city_path = "test_data/Vaugneray"
-    log_file = "test_data/logs/features.log"
+def _calculate_building_features(buildings):
+        buildings['footprint_area'] = buildings.geometry.area
+        buildings['perimeter'] = buildings.geometry.length
+        buildings['phi'] = building.calculate_phi(buildings)
+        buildings['longestAxisLength'] = longest_axis_length(buildings)
+        buildings['elongation'] = elongation(buildings)
+        buildings['convexity'] = convexity(buildings)
+        buildings['orientation'] = orientation(buildings)
+        buildings['corners'] = corners(buildings)
+        buildings['shared_wall_length'] = shared_walls(buildings)
+        buildings['touches'] = building.calculate_touches(buildings)
+
+        return buildings
+
+
+def _calculate_building_buffer_features(buildings):
+    buffer_fts = {
+        'avg_footprint_area': ('footprint_area', 'mean'),
+        'std_footprint_area': ('footprint_area', 'std'),
+        'avg_elongation': ('elongation', 'mean'),
+        'std_elongation': ('elongation', 'std'),
+        'avg_convexity': ('convexity', 'mean'),
+        'std_convexity': ('convexity', 'std'),
+        'avg_orientation': ('orientation', 'mean'),
+        'std_orientation': ('orientation', 'std'),
+        'total_footprint_area': ('footprint_area', 'sum'),
+        'n_buildings': ('footprint_area', 'count'),
+    }
+    buildings = buffer.calculate_h3_buffer_features(buildings, buffer_fts, 10, [1, 4])  # corresponds to a buffer of 0.1 and 0.9 km^2
+
+    return buildings
+
+
+def _calculate_street_features(buildings, streets):
+    buildings[['size_of_closest_street', 'distance_to_closest_street']] = street.street_size_and_distance_to_closest_street(buildings, streets)
+
+    return buildings
+
+
+if __name__ == '__main__':
+    city_path = 'test_data/Vaugneray'
+    log_file = 'test_data/logs/features.log'
     execute_feature_pipeline(city_path, log_file)
--- util/data.py
+++ util/data.py
@@ -2,7 +2,6 @@ import pandas as pd
 import geopandas as gpd
 from shapely import wkt
 from pathlib import Path
-from typing import Optional
 
 CRS_UNI = 'EPSG:3035'
 geometry_col = 'geometry'
@@ -24,3 +23,9 @@ def load_buildings(city_path: str) -> gpd.GeoDataFrame:
     buffer_ = load_csv(Path(f"{city_path}/{city_name}_buffer.csv"))
     buildings = gpd.pd.concat([buildings, buffer_], ignore_index=True)
     return buildings
+
+
+def load_streets(city_path: str) -> gpd.GeoDataFrame:
+    city_name = city_path.split("/")[-1]
+    streets = load_csv(Path(f"{city_path}/{city_name}_streets_raw.csv"))
+    return streets
