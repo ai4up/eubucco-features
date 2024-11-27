870ab25887280c58469757c310e3066e4ebcdabc
Florian Nachtigall <nachtigall.florian@gmail.com>
2024-11-27 19:08:32 +0100

    Add data loading & storing util funcs

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,4 @@
-from .data import load_csv, load_buildings, load_streets
+from .data import load_csv, load_buildings, load_osm_buildings, load_streets, load_pois, store_features
 from .spatial import sjoin_nearest_cols
 
-__all__ = ['load_csv', 'load_buildings', 'load_streets', 'sjoin_nearest_cols']
+__all__ = ['load_csv', 'load_buildings', 'load_osm_buildings', 'load_streets', 'load_pois', 'store_features', 'sjoin_nearest_cols']
--- util/data.py
+++ util/data.py
@@ -29,3 +29,20 @@ def load_streets(city_path: str) -> gpd.GeoDataFrame:
     city_name = city_path.split("/")[-1]
     streets = load_csv(Path(f"{city_path}/{city_name}_streets_raw.csv"))
     return streets
+
+
+def load_pois(city_path: str) -> gpd.GeoDataFrame:
+    city_name = city_path.split("/")[-1]
+    pois = gpd.read_file(Path(f"{city_path}/{city_name}_pois.gpkg"))
+    return pois
+
+
+def load_osm_buildings(city_path: str) -> gpd.GeoDataFrame:
+    city_name = city_path.split("/")[-1]
+    buildings = gpd.read_file(Path(f"{city_path}/{city_name}_osm_bldgs.gpkg"))
+    return buildings
+
+
+def store_features(buildings: gpd.GeoDataFrame, city_path: str):
+    city_name = city_path.split("/")[-1]
+    buildings.to_file(Path(f"{city_path}/{city_name}_features.gpkg"), driver='GPKG')
