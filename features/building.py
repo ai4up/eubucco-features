dcee69e87a24369f4fabe4911171b78725c26753
Florian Nachtigall <nachtigall.florian@gmail.com>
2024-11-28 11:17:51 +0100

    Add type hints for building features

--- features/building.py
+++ features/building.py
@@ -2,22 +2,24 @@ import math
 
 import numpy as np
 import geopandas as gpd
+import pandas as pd
 
-def calculate_phi(buildings):
+
+def calculate_phi(buildings: gpd.GeoDataFrame) -> pd.Series:
     max_dist = buildings.geometry.map(lambda g: g.centroid.hausdorff_distance(g.exterior))
     circle_area = buildings.centroid.buffer(max_dist).area
     return buildings.area / circle_area
 
 
-def calculate_touches(buildings):
+def calculate_touches(buildings: gpd.GeoDataFrame) -> pd.Series:
     touching_pairs = gpd.sjoin(buildings, buildings, predicate='intersects')
     touches = touching_pairs.groupby('id_left').size() - 1
     return buildings['id'].map(touches).fillna(0).astype(int)
 
 
-def calculate_norm_perimeter(buildings):
+def calculate_norm_perimeter(buildings: gpd.GeoDataFrame) -> pd.Series:
     return _circle_perimeter(buildings.area) / buildings.length
 
 
-def _circle_perimeter(area):
+def _circle_perimeter(area: pd.Series) -> pd.Series:
     return 2 * np.sqrt(area * math.pi)
