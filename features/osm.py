2349a029b3283d234998e0579d34dbe64e7fb5ab
Florian Nachtigall <nachtigall.florian@gmail.com>
2024-12-20 16:23:32 +0100

    Use osm_type_matches_v1.csv to determine nearest OSM building types

--- features/osm.py
+++ features/osm.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, List, Tuple, Union
 
 import geopandas as gpd
@@ -5,35 +6,17 @@ import pandas as pd
 
 import util
 
+module_dir = os.path.dirname(__file__)
+BUILDING_TYPE_CATEGORIES_FILE = os.path.join(module_dir, "..", "data", "osm_type_matches_v1.csv")
+
 
 def load_osm_buildings(buildings_dir: str, region_id: str) -> gpd.GeoDataFrame:
-    # according to https://wiki.openstreetmap.org/wiki/Key:building (except for additional category for industrial)
-    # fmt: off
-    categories = {
-        "accommodation": ["apartments", "barracks", "bungalow", "cabin", "detached", "annexe", "dormitory", "farm", "ger", "hotel", "house", "houseboat", "residential", "semidetached_house", "static_caravan", "stilt_house", "terrace", "tree_house", "trullo"],  # noqa: E501
-        "commercial": ["commercial", "kiosk", "office", "retail", "supermarket", "warehouse"],
-        "industrial": ["industrial"],
-        "religious": ["religious", "cathedral", "chapel", "church", "kingdom_hall", "monastery", "mosque", "presbytery", "shrine", "synagogue", "temple"],  # noqa: E501
-        "civic_amenity": ["bakehouse", "bridge", "civic", "college", "fire_station", "government", "gatehouse", "hospital", "kindergarten", "museum", "public", "school", "toilets", "train_station", "transportation", "university"],  # noqa: E501
-        "agricultural": ["barn", "conservatory", "cowshed", "farm_auxiliary", "greenhouse", "slurry_tank", "stable", "sty", "livestock", "grandstand", "pavilion", "riding_hall", "sports_hall", "sports_centre", "stadium"],  # noqa: E501
-        "storage": ["allotment_house", "boathouse", "hangar", "hut", "shed"],
-        "cars": ["carport", "garage", "garages", "parking"],
-        "technical_buildings": ["digester", "service", "tech_cab", "transformer_tower", "water_tower", "storage_tank", "silo"],  # noqa: E501
-        "other_buildings": ["beach_hut", "bunker", "castle", "construction", "container", "guardhouse", "military", "pagoda", "quonset_hut", "roof", "ruins", "tent", "tower", "windmill", "yes"],  # noqa: E501
-    }
-    custom_categories = {
-        "residential": categories["accommodation"],
-        "industrial": categories["industrial"],
-        "commercial": categories["commercial"] + categories["civic_amenity"],
-        "agricultural": categories["agricultural"],
-        "others": categories["other_buildings"] + categories["technical_buildings"] + categories["cars"] + categories["storage"] + categories["civic_amenity"] + categories["religious"],  # noqa: E501
-    }
-    # fmt: on
+    categories = pd.read_csv(BUILDING_TYPE_CATEGORIES_FILE)
+    categories_map = categories.set_index("type_source")["type"].to_dict()
 
-    custom_categories = {v: k for k, values in custom_categories.items() for v in values}
     buildings = util.load_buildings(buildings_dir, region_id)
     buildings["height"] = pd.to_numeric(buildings["height"], errors="coerce")
-    buildings["type"] = buildings["type_source"].map(custom_categories)
+    buildings["type"] = buildings["type_source"].map(categories_map)
 
     return buildings[["geometry", "height", "type"]]
 
