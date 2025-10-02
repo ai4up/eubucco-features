ai4up/eubucco-features

commit cd287df6ed3c57e3d6263865868c42dd710fd54f

Optimize buffer feature calculations by restricting to relevant H3 cells
Previously, buffer features were computed for all H3 cells, including
those outside the region of interest (i.e. where no buildings exist).
This commit introduces the option to pass a set of H3 indices of
interest (derived from the target buildings), so buffer and neighborhood
aggregations are only computed where needed.

Changes:
- Updated buffer functions to accept optional `grid_cells`
- Modified pipeline buffer feature steps to restrict calculations to
  building H3 indices
- Minorly adjusted type hints and docstrings

features/pipeline.py
@@ -375,7 +375,8 @@ def _calculate_poi_buffer_features(buildings: gpd.GeoDataFrame, pois_dir: str, r
     pois = pois.to_crs(buildings.crs)
 
     buffer_fts = {"poi_n": ("amenity", "count")}
-    hex_grid = buffer.calculate_h3_buffer_features(pois, buffer_fts, H3_RES, H3_BUFFER_SIZES)
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    hex_grid = buffer.calculate_h3_buffer_features(pois, buffer_fts, H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid)
 
     hex_grid_large_buffer = hex_grid[hex_grid.columns[-1]]
@@ -385,12 +386,13 @@ def _calculate_poi_buffer_features(buildings: gpd.GeoDataFrame, pois_dir: str, r
 
 
 def _calculate_osm_buildings_buffer_features(
-    buildings: gpd.GeoDataFrame, osm_bldgs_dir: str, region_id: str
+    buildings: gpd.GeoDataFrame, osm_bldgs_dir: str, osm_buffer_dir:str, region_id: str
 ) -> gpd.GeoDataFrame:
-    osm_buildings = osm.load_osm_buildings(osm_bldgs_dir, region_id)
+    osm_buildings = osm.load_osm_buildings(osm_bldgs_dir, osm_buffer_dir, region_id)
     osm_buildings = osm_buildings.to_crs(buildings.crs)
 
-    hex_grid_type_shares = buffer.calculate_h3_buffer_shares(osm_buildings, "type", H3_RES, H3_BUFFER_SIZES)
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    hex_grid_type_shares = buffer.calculate_h3_buffer_shares(osm_buildings, "type", H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     hex_grid_type_shares = hex_grid_type_shares.add_prefix("osm_type_share_")
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid_type_shares)
 
@@ -400,7 +402,7 @@ def _calculate_osm_buildings_buffer_features(
         "osm_max_height": ("height", "max"),
         "osm_type_variety": ("type", "nunique"),
     }
-    hex_grid = buffer.calculate_h3_buffer_features(osm_buildings, buffer_fts, H3_RES, H3_BUFFER_SIZES)
+    hex_grid = buffer.calculate_h3_buffer_features(osm_buildings, buffer_fts, H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid)
 
     return buildings
@@ -411,7 +413,8 @@ def _calculate_GHS_built_up_buffer_features(buildings: gpd.GeoDataFrame, built_u
     built_up = load_GHS_built_up(built_up_file, area)
     built_up = built_up.to_crs(buildings.crs)
 
-    hex_grid_type_shares = buffer.calculate_h3_buffer_shares(built_up, "use_type", H3_RES, H3_BUFFER_SIZES)
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    hex_grid_type_shares = buffer.calculate_h3_buffer_shares(built_up, "use_type", H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     hex_grid_type_shares = hex_grid_type_shares.add_prefix("ghs_use_type_share_")
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid_type_shares)
 
@@ -420,7 +423,7 @@ def _calculate_GHS_built_up_buffer_features(buildings: gpd.GeoDataFrame, built_u
         "ghs_greenness": ("NDVI", "mean"),
         "ghs_height": ("height", "mean"),
     }
-    hex_grid = buffer.calculate_h3_buffer_features(built_up, buffer_fts, H3_RES, H3_BUFFER_SIZES)
+    hex_grid = buffer.calculate_h3_buffer_features(built_up, buffer_fts, H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid)
 
     return buildings

features/buffer.py
@@ -1,6 +1,7 @@
 from collections.abc import Iterable
 from typing import Callable, Dict, List, Tuple, Union
 
+import pandas as pd
 import geopandas as gpd
 import h3
 import h3pandas  # noqa
@@ -11,7 +12,7 @@ from shapely import Point
 
 def aggregate_to_h3_grid(
     gdf: gpd.GeoDataFrame, operation: Dict[str, Tuple[str, Callable]], res: int
-) -> gpd.GeoDataFrame:
+) -> pd.DataFrame:
     """
     Aggregates a GeoDataFrame to a hexagonal H3-indexed grid.
 
@@ -23,7 +24,7 @@ def aggregate_to_h3_grid(
         res: Resolution of the hexagonal grid.
 
     Returns:
-        Aggregated GeoDataFrame.
+        Aggregated DataFrame.
     """
     if "h3_index" not in gdf.columns:
         gdf["h3_index"] = h3_index(gdf, res)
@@ -43,7 +44,7 @@ def calculate_h3_grid_shares(gdf: gpd.GeoDataFrame, col: str, res: int) -> gpd.G
         res: The resolution of the H3 hexagonal grid.
 
     Returns:
-        A GeoDataFrame with the unique value shares of the specified column within each H3 hexagonal grid cell.
+        A DataFrame with the unique value shares of the specified column within each H3 hexagonal grid cell.
     """
     if "h3_index" not in gdf.columns:
         gdf["h3_index"] = h3_index(gdf, res)
@@ -54,7 +55,7 @@ def calculate_h3_grid_shares(gdf: gpd.GeoDataFrame, col: str, res: int) -> gpd.G
 
 
 def calculate_h3_buffer_features(
-    gdf: gpd.GeoDataFrame, operation: Dict[str, Tuple[str, Callable]], res: int, k: Union[int, List[int]]
+    gdf: gpd.GeoDataFrame, operation: Dict[str, Tuple[str, Callable]], res: int, k: Union[int, List[int]], grid_cells: pd.DataFrame = None
 ) -> gpd.GeoDataFrame:
     """
     Calculate buffer features for a GeoDataFrame based on H3 indexes.
@@ -67,25 +68,30 @@ def calculate_h3_buffer_features(
         res: H3 resolution level.
         k:  The number of hexagonal rings to include in the buffer. Provide a list to calculate features
             for multiple buffer sizes.
+        grid_cells: Optional list of H3 indexes to calculate features for.
 
     Returns:
         A hexagonal grid with the calculated buffer features.
     """
-    hex_grid = aggregate_to_h3_grid(gdf, operation, res)
+    grid_values = aggregate_to_h3_grid(gdf, operation, res)
+    if grid_cells is None:
+        grid_cells = grid_values
     nbh_operation = _determine_neighborhood_agg_operation(operation)
-    hex_grid = _calcuate_hex_rings_aggregate(hex_grid, nbh_operation, res, k)
+    agg_grid = _calcuate_hex_rings_aggregate(grid_cells, grid_values, nbh_operation, res, k)
 
-    return hex_grid
+    return agg_grid
 
 
 def calculate_h3_buffer_shares(
-    gdf: gpd.GeoDataFrame, col: str, h3_res: int, k: Union[int, List[int]]
+    gdf: gpd.GeoDataFrame, col: str, h3_res: int, k: Union[int, List[int]], grid_cells: pd.DataFrame = None
 ) -> gpd.GeoDataFrame:
-    hex_grid_shares = calculate_h3_grid_shares(gdf, col, h3_res)
-    hex_grid_shares = hex_grid_shares.unstack(level=col, fill_value=0)
-    hex_grid_shares = _calcuate_hex_rings_aggregate(hex_grid_shares, "mean", h3_res, k)
+    grid_shares = calculate_h3_grid_shares(gdf, col, h3_res)
+    grid_shares = grid_shares.unstack(level=col, fill_value=0)
+    if grid_cells is None:
+        grid_cells = grid_shares
+    agg_grid_shares = _calcuate_hex_rings_aggregate(grid_cells, grid_shares, "mean", h3_res, k)
 
-    return hex_grid_shares
+    return agg_grid_shares
 
 
 def h3_index(gdf: Union[gpd.GeoSeries, gpd.GeoDataFrame], res: int) -> List[str]:
@@ -133,14 +139,14 @@ def ft_suffix(res: int, k: int = 0) -> str:
 
 
 def _calcuate_hex_rings_aggregate(
-    hex_grid: gpd.GeoDataFrame, operation: Union[str, List, Dict, Callable], res: int, k: Union[int, List[int]]
-) -> gpd.GeoDataFrame:
+    grid_cells: pd.DataFrame, grid_values: pd.DataFrame, operation: Union[str, List, Dict, Callable], res: int, k: Union[int, List[int]]
+) -> pd.DataFrame:
     aggregates = []
     hex_rings = _ensure_iterable(k)
 
     # Calculate aggregate for each hex ring size / buffer size
     for j in hex_rings:
-        ring_aggregate = _calcuate_hex_ring_aggregate(hex_grid, operation, j)
+        ring_aggregate = _calcuate_hex_ring_aggregate(grid_cells, grid_values, operation, j)
         ring_aggregate = ring_aggregate.add_suffix("_" + ft_suffix(res, j))
         aggregates.append(ring_aggregate)
 
@@ -148,16 +154,16 @@ def _calcuate_hex_rings_aggregate(
 
 
 def _calcuate_hex_ring_aggregate(
-    gdf: gpd.GeoDataFrame, operation: Union[str, List, Dict, Callable], k: int
-) -> gpd.GeoDataFrame:
+    grid_cells: pd.DataFrame, grid_values: pd.DataFrame, operation: Union[str, List, Dict, Callable], k: int
+) -> pd.DataFrame:
     # Add column with neighboring hexagons
-    neighbors = gdf.h3.k_ring(k=k)["h3_k_ring"]
+    neighbors = grid_cells.h3.k_ring(k=k)["h3_k_ring"]
 
     # Add self to the neighbor list
     neighbors[:] = [[i] + n for i, n in zip(neighbors.index, neighbors)]
 
     # Perform aggregate operation (e.g. mean) across the hexagons in the neighborhood
-    agg = neighbors.apply(lambda x: gdf.reindex(x).agg(operation))
+    agg = neighbors.apply(lambda x: grid_values.reindex(x).agg(operation))
 
     return agg
 

commit c8f200ec2bcd954d53d645f663a5220cdf275f7e

Fix calculation of buffer features
Duplicate h3 grid cells resulted in an explosion of the buildings
dataframe. Bug introduced in cd287df.

features/pipeline.py
@@ -376,7 +376,7 @@ def _calculate_building_buffer_features(buildings: gpd.GeoDataFrame) -> gpd.GeoD
             suffix = buffer.ft_suffix(H3_RES, s)
             buildings[f"{cat}_diff_{ft}_{suffix}"] = buildings[f"{cat}_avg_{ft}_{suffix}"] - buildings[f"{cat}_{ft}"]
 
-    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'].unique())
     hex_grid_type_shares = buffer.calculate_h3_buffer_shares(buildings, "bldg_type", H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest, dropna=True, n_min=5)
     hex_grid_type_shares = hex_grid_type_shares.add_prefix("bldg_type_share_")
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid_type_shares)
@@ -396,7 +396,7 @@ def _calculate_poi_buffer_features(buildings: gpd.GeoDataFrame, pois_dir: str, r
     pois = pois.to_crs(buildings.crs)
 
     buffer_fts = {"poi_n": ("amenity", "count")}
-    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'].unique())
     hex_grid = buffer.calculate_h3_buffer_features(pois, buffer_fts, H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid)
 
@@ -411,7 +411,7 @@ def _calculate_GHS_built_up_buffer_features(buildings: gpd.GeoDataFrame, built_u
     built_up = load_GHS_built_up(built_up_file, area)
     built_up = built_up.to_crs(buildings.crs)
 
-    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'])
+    h3_indices_of_interest = pd.DataFrame(index=buildings['h3_index'].unique())
     hex_grid_type_shares = buffer.calculate_h3_buffer_shares(built_up, "use_type", H3_RES, H3_BUFFER_SIZES, h3_indices_of_interest)
     hex_grid_type_shares = hex_grid_type_shares.add_prefix("ghs_use_type_share_")
     buildings = _add_grid_fts_to_buildings(buildings, hex_grid_type_shares)